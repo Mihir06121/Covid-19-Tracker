8f576288dde76cb331ab180af5566d4275231fda
Mihir06121 <mp06121@gmail.com>
2020-08-04 00:57:03 +0530

    Updates

---
+++ precache-manifest.d3c5e8fe4e8c9ff1153320dfa180560c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3377f9f87e967b09fc98c0b4f0aeda3f",
+    "url": "/Mihir06121/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "b0803c9404c0e9cac4ef",
+    "url": "/Mihir06121/Covid-19-Tracker/static/css/main.2fc0fc28.chunk.css"
+  },
+  {
+    "revision": "54a788ffd1e39e646548",
+    "url": "/Mihir06121/Covid-19-Tracker/static/js/2.cd7f60eb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Mihir06121/Covid-19-Tracker/static/js/2.cd7f60eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0803c9404c0e9cac4ef",
+    "url": "/Mihir06121/Covid-19-Tracker/static/js/main.a08cc3e8.chunk.js"
+  },
+  {
+    "revision": "74a1f9f63c660ed18bec",
+    "url": "/Mihir06121/Covid-19-Tracker/static/js/runtime-main.b0c980df.js"
+  }
+]);
